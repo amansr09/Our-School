2f558e01f6ed14dba34ef64faf2279fa66ce4f5f
Aman Srivastava <aman.s@juspay.in>
2025-10-23 21:59:01 +0530

    Hardcode Railway API URL for mobile login support

--- school-website/client/src/utils/api.js
+++ school-website/client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://our-school-production-b756.up.railway.app/api',
 });
 
 // Add token to requests
